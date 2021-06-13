0c7abf6864e286844b37faeb0b732e4922504051
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-13 22:15:11 +0530

    animate flash button

--- game.js
+++ game.js
@@ -1,11 +1,17 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
-function nextSequence(){
-    let randomN = Math.floor(Math.random() * 4);
-    //console.log(randomN);
-    return randomN;
+function nextSequence() {
+  let randomN = Math.floor(Math.random() * 4);
+  //console.log(randomN);
+  return randomN;
 }
 let randomChosenColour = nextSequence();
 gamePattern.push(randomChosenColour);
 console.log(gamePattern);
+console.log(buttonColours[randomChosenColour]);
+
+$(`.${buttonColours[randomChosenColour]}`).addClass("pressed");
+setTimeout(function() {
+    $(`.${buttonColours[randomChosenColour]}`).removeClass("pressed");
+}, 100);
