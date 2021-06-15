cdfee3634f028a00cbdbd8faf267946d4bef41f4
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-15 11:50:10 +0530

    add sounds to the website

--- game.js
+++ game.js
@@ -2,16 +2,18 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence() {
-  let randomN = Math.floor(Math.random() * 4);
-  //console.log(randomN);
-  return randomN;
-}
-let randomChosenColour = nextSequence();
-gamePattern.push(randomChosenColour);
-console.log(gamePattern);
-console.log(buttonColours[randomChosenColour]);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-$(`.${buttonColours[randomChosenColour]}`).addClass("pressed");
-setTimeout(function() {
-    $(`.${buttonColours[randomChosenColour]}`).removeClass("pressed");
-}, 100);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+
+$(document).keypress(function() {
+  nextSequence();
+});
